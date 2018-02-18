ee5ee7564b7cc99daafff1f0ee259467366e187d
KyleDavidE <Kyle.Ehrlich@gmail.com>
2018-02-18 14:01:02 -0500

    fix dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,9 @@
 
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const path = require('path');
 const common = require('./webpack.common.js');
-module.exports = merge({
+module.exports = merge(common,{
     devServer: {
         contentBase: path.join(__dirname, "public")
     },
